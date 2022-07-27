b8fa977a70e6d64c1f170135a9b68e4a094d93b9
Loai Qubti <loai17@meet.mit.edu>
2022-07-27 15:32:04 +0300

    Added screenshot funcitonality

---
+++ Assets/ScreenshotHandler.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class ScreenshotHandler : MonoBehaviour
+{
+
+    private IEnumerator Screenshot() 
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        texture.Apply();
+
+        // PC
+        byte[] bytes = texture.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/Screenshot.png", bytes);
+        Debug.Log("Took screenshot");
+        Destroy(texture);
+    }
+
+    public void TakeScreenshot() 
+    {
+        StartCoroutine("Screenshot");
+    }
+
+
+
+
+
+ /*   private static ScreenshotHandler instance;
+
+    private Camera myCamera;
+    private bool takeScreenshotOnNextFrame;
+
+    private void Awake()
+    {
+        instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TakeScreenshot_Static(Screen.width, Screen.height);
+        }
+    }
+
+    private void OnPostRender()
+    {
+        if (takeScreenshotOnNextFrame) {
+            takeScreenshotOnNextFrame = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenshot.png",byteArray);
+            Debug.Log("Saved CameraScreenshot.png");
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            myCamera.targetTexture = null;
+        }
+    }
+
+    private void TakeScreenshot(int width, int height) {
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenshotOnNextFrame = true;
+    }
+
+    public static void TakeScreenshot_Static(int width, int height) {
+        instance.TakeScreenshot(width, height);
+    }*/
+}

aaf60d90c2e4c546bf9f5589d7f6d833598bdc12
Yannik Hupfer <yh@colognegamelab.de>
2022-09-07 18:43:51 +0200

    Fixed zooming issue

--- Assets/Scripts/MultiTouchScrollRect.cs
+++ Assets/Scripts/MultiTouchScrollRect.cs
@@ -6,38 +6,24 @@ using UnityEngine.UI;
 
 public class MultiTouchScrollRect : ScrollRect
 {
-    // https://stackoverflow.com/questions/56221113/fix-for-scrollrect-multi-touch-in-unity
-    // https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/wiki/Controls/MultiTouchScrollRect
-    // Store the id of the touch.
-    private int pid = -100;
-
     public override void OnBeginDrag(PointerEventData eventData)
     {
         if(Input.touchCount == 1)
         {
-            // Only store the id of the first touch
-            pid = eventData.pointerId;
             base.OnBeginDrag(eventData);
-        }
-        else
-        {
-            pid = -100;
-            base.StopMovement();
         }
     }
 
     public override void OnDrag(PointerEventData eventData)
     {
-        if(pid == eventData.pointerId)
+        if(Input.touchCount == 1)
         {
-            // Only move if it's only one touch
             base.OnDrag(eventData);
         }
     }
 
     public override void OnEndDrag(PointerEventData eventData)
     {
-        pid = -100;
         base.OnEndDrag(eventData);
     }
 }

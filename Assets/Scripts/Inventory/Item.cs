2f51f44e39ece9c2a75fd49badb725fcbc0f608b
Yannik Hupfer <yh@colognegamelab.de>
2022-09-16 17:34:23 +0200

    Added tooltips

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewItem", menuName = "ScriptableObjects/Item", order = 1)]
 public class Item : ScriptableObject
 {
+    [Tooltip("The name of the item")]
     public string Name;
+    [Tooltip("The description of the item")]
     public string Description;
+    [Tooltip("The normal sprite used in the inventory")]
     public Sprite sprite;
+    [Tooltip("The sprite that should be used when it has outlines (transfered in the shop and waiting that the player accepts or rejects)")]
     public Sprite ghostSprite;
+    [Tooltip("The price of the item")]
     public int Price;
+    [Tooltip("The number of slots it occupies (should be 1 or 2)")]
     public int Volume = 1;
+    [Tooltip("The maximum stack count. 1 for not stackable, 0 for infinite stackable.")]
     public int MaxStackCount = 1;
 
     public bool IsStackable

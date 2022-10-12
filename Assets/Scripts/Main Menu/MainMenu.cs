9775a01d93e627dc1c7cb4852581338032731887
Yannik Hupfer <yh@colognegamelab.de>
2022-10-12 18:08:48 +0200

    Added documentation to main menu

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * To open the diary, call MainMenu::OpenDiary with the page you want opened as the argument.
+ * To close the diary, call MainMenu::CloseDiary.
+ * 
+ * MAIN MENU PAGES
+ * The prefab MainMenuPage is a prefab for a double sided page of the diary in the main menu.
+ * Place a new page under Main Menu > Diary > Content.
+ * You can create groups with empty game objects (e.g. NewGame for the new game button of the cover).
+ * Once added, you can add your content to the left or right page.
+ * If you want to switch pages and have a button somewhere, you can add a call to MainMenu::OpenNextPage or OpenPreviousPage,
+ * depending on if you want to scroll to the right or left, with the animator of the new page as the argument.
+ */
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]

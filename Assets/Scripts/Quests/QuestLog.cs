164b8b091edbf3cc1d3e3625f95f6b924c93ebaf
Yannik Hupfer <yh@colognegamelab.de>
2022-11-22 17:26:01 +0100

    Added quest log skeleton

---
+++ Assets/Scripts/Quests/QuestLog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestLog : MonoBehaviour
+{
+    [SerializeField]
+    private QuestsPanel mainQuestsPanel;
+    [SerializeField]
+    private QuestsPanel sideQuestsPanel;
+}

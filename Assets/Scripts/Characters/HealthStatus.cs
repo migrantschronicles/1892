f8442e9a724a19c3b82033337f0fcb7899a363f1
Yannik Hupfer <yannikhupfer@gmail.com>
2023-01-05 15:45:45 +0100

    Basic implementation of hungry

--- Assets/Scripts/Characters/HealthStatus.cs
+++ Assets/Scripts/Characters/HealthStatus.cs
@@ -12,32 +12,35 @@ public class EndOfDayHealthData
 
 public class HealthStatus_Hungry
 {
-    // Stored so we can retrieve if the character has cholera (double food amount).
-    private CharacterHealthStatus status;
     private int requiredFoodAmount = 0;
-
-    public HealthStatus_Hungry(CharacterHealthStatus status)
-    {
-        this.status = status;
-    }
+    
+    public int DaysWithoutEnoughFood { get; private set; }
 
     public void OnEndOfDay(int receivedFoodAmount)
     {
+        // Every day the character needs the double amount plus one for this day.
         requiredFoodAmount = requiredFoodAmount * 2 + 1;
+        // Subtract the amount of food the character has received today.
         requiredFoodAmount = Mathf.Max(0, requiredFoodAmount - receivedFoodAmount);
+
+        if(requiredFoodAmount == 0)
+        {
+            // The character has eaten enough for today and also refilled the missing foods that accumulated the last days.
+            DaysWithoutEnoughFood = 0;
+        }
+        else
+        {
+            // The character has not received enough food for the day or to refeed the missing food amounts from the last days.
+            ++DaysWithoutEnoughFood;
+        }
     }
 }
 
 public class CharacterHealthStatus
 {
-    private HealthStatus_Hungry hungryStatus;
+    private HealthStatus_Hungry hungryStatus = new HealthStatus_Hungry();
 
     public CharacterHealthData CharacterData { get; private set; }
-
-    public CharacterHealthStatus()
-    {
-        hungryStatus = new HealthStatus_Hungry(this);
-    }
 
     public void Init(CharacterHealthData characterData)
     {
@@ -78,6 +81,7 @@ public class HealthStatus
             }
 
             status.OnEndOfDay(healthData);
+            handledCharacters.Add(healthData.name);
         }
 
         if(handledCharacters.Count < characters.Count)

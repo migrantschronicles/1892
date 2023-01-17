5a56689520d73df43de7a4745cea0de480942b48
Loai Qubti <loai17@meet.mit.edu>
2023-01-17 15:20:45 +0100

    a

--- Assets/ClockManager.cs
+++ Assets/ClockManager.cs
@@ -24,16 +24,6 @@ public class ClockManager : MonoBehaviour
         hourHandle.rotation = Quaternion.Euler(0, 0, hourHandle.rotation.z - (hour * (360 / 12) + (minutes * 0.5f)) + hourOffset);
 
         // Moving the knob with the day.
-        /*float knobY = 92f - ((92f - 10.5f) / NewGameManager.Instance.hoursPerDay);
-        float knobX = 120f;
-        if (hour <= (NewGameManager.Instance.hoursPerDay/2))
-        {
-            knobX = 120f + hour * ((140f - 120f) / (NewGameManager.Instance.hoursPerDay/2));
-        }
-        else { knobX = 140f - hour * ((140f - 120f) / (NewGameManager.Instance.hoursPerDay / 2)); }
-
-        knob.SetLocalPositionAndRotation(new Vector3(knobX, knobY, 0), knob.rotation);*/
-
         Vector3 startPoint = new Vector3(120, 92, 0);
         Vector3 endPoint = new Vector3(120, 10.5f, 0);
         Vector3 controlPoint1 = new Vector3(160, 51.25f, 0);

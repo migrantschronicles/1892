6023727dd72264bdad13a9f67b8c82fc6d3b7dbe
Yannik Hupfer <yannikhupfer@gmail.com>
2023-02-02 09:51:02 +0100

    Automatically set foreground object height to dialog height

--- Assets/Scripts/UI/ForegroundScene.cs
+++ Assets/Scripts/UI/ForegroundScene.cs
@@ -33,11 +33,64 @@ public class ForegroundScene : MonoBehaviour
         RectTransform canvasRect = LevelInstance.Instance.CanvasRect;
         float marginSide = UI_SIZE.x / 2 + dialogRect.rect.xMin;
 
-        Vector2 leftUIPosition = new Vector2(dialogRect.rect.xMin - marginSide / 2, 0);
-        Vector2 rightUIPosition = new Vector2(dialogRect.rect.xMax + marginSide / 2, 0);
+        Vector2 leftUIPosition = new(dialogRect.rect.xMin - marginSide / 2, 0);
+        Vector2 rightUIPosition = new(dialogRect.rect.xMax + marginSide / 2, 0);
 
         left.position = canvasRect.TransformPoint(leftUIPosition);
         right.position = canvasRect.TransformPoint(rightUIPosition);
+
+        // Update scale
+        Vector2 localMin = new(0, dialogRect.rect.yMin);
+        Vector2 localMax = new(0, dialogRect.rect.yMax);
+        Vector3 worldMin = canvasRect.TransformPoint(localMin);
+        Vector3 worldMax = canvasRect.TransformPoint(localMax);
+        float worldHeight = worldMax.y - worldMin.y;
+
+        if(leftCharacter)
+        {
+            SetScaleToWorldHeight(leftCharacter, worldHeight);
+        }
+
+        if (rightCharacter)
+        {
+            SetScaleToWorldHeight(rightCharacter, worldHeight);
+        }
+    }
+
+    private void SetScaleToWorldHeight(GameObject character, float worldHeight)
+    {
+        // Detach parent so that scale does not modify result.
+        Transform parent = character.transform.parent;
+        character.transform.SetParent(null, false);
+
+        // Calculate the sprite height (since the characters are made of a lot of sprites, all have to be considered).
+        float spriteHeight = CalculateSpriteHeight(character);
+        float scaleFactor = worldHeight / spriteHeight;
+        parent.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+        // Reparent
+        character.transform.SetParent(parent, false);
+    }
+
+    private float CalculateSpriteHeight(GameObject go)
+    {
+        float minY = 0;
+        float maxY = 0;
+        SpriteRenderer[] sprites = go.GetComponentsInChildren<SpriteRenderer>();
+        for (int i = 0; i < sprites.Length; ++i)
+        {
+            SpriteRenderer sprite = sprites[i];
+            if(i == 0 || sprite.bounds.min.y < minY)
+            {
+                minY = sprite.bounds.min.y;
+            }
+            if(i == 0 || sprite.bounds.max.y > maxY)
+            {
+                maxY = sprite.bounds.max.y;
+            }
+        }
+
+        return maxY - minY;
     }
 
     public void SetCharacters(GameObject leftPrefab, GameObject rightPrefab)
@@ -61,6 +114,7 @@ public class ForegroundScene : MonoBehaviour
                 character = Instantiate(prefab, parent);
                 SetLayer(character, LayerMask.NameToLayer("Foreground"));
                 animController = character.GetComponent<IAnimationController>();
+                UpdatePosition();
             }
         }
     }

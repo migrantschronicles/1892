711bcb2be4ea54c93fd7219492eb43112dedd2ff
Yannik Hupfer <yannikhupfer@gmail.com>
2023-02-14 16:46:22 +0100

    Removed comments

--- Assets/LoadingManager.cs
+++ Assets/LoadingManager.cs
@@ -36,41 +36,4 @@ public class LoadingManager : MonoBehaviour
             yield return null;
         }
     }
-
-    /*
-    private bool loadInitiated = false;
-    private string loadLevel;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!loadInitiated) 
-        {
-            loadLevel = LevelInstance.Instance.LocationName;
-            LoadNextLevel(loadLevel);
-            loadInitiated = true;
-        }
-    }
-
-    public void LoadNextLevel(string name) 
-    {
-        StartCoroutine(WaitXSeconds(3));
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        SceneManager.LoadScene(sceneName: name);
-    }
-
-    private void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, LoadSceneMode mode)
-    {
-        if(scene.name == loadLevel)
-        {
-            SceneManager.sceneLoaded -= OnSceneLoaded;
-            NewGameManager.Instance.PostLevelLoad();
-        }
-    }
-
-    public IEnumerator WaitXSeconds(float seconds) 
-    {
-        yield return new WaitForSeconds(seconds);
-    }
-    */
 }

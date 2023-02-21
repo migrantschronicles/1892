6ddf4b3677913e6d2c9d482be22646d332c91574
Yannik Hupfer <yannikhupfer@gmail.com>
2023-02-21 10:15:49 +0100

    Localized transportation methods in DiscoverRoutePopup

--- Assets/Scripts/UI/Popups/DiscoveredRoutePopup.cs
+++ Assets/Scripts/UI/Popups/DiscoveredRoutePopup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.Localization.Components;
 using UnityEngine.UI;
 
@@ -9,6 +10,18 @@ public class DiscoveredRoutePopup : MonoBehaviour, IPopup
 {
     [SerializeField]
     private Text title;
+    [SerializeField]
+    private LocalizedString walkingString;
+    [SerializeField]
+    private LocalizedString tramString;
+    [SerializeField]
+    private LocalizedString trainString;
+    [SerializeField]
+    private LocalizedString cartString;
+    [SerializeField]
+    private LocalizedString shipString;
+    [SerializeField]
+    private LocalizedString carriageString;
 
     public event IPopup.OnPopupAction OnAccepted;
 
@@ -17,9 +30,25 @@ public class DiscoveredRoutePopup : MonoBehaviour, IPopup
         OnAccepted?.Invoke(this);
     }
 
+    private string GetLocalizedStringForMethod(TransportationMethod method)
+    {
+        LocalizedString localizedString = null;
+        switch (method)
+        {
+            case TransportationMethod.Walking: localizedString = walkingString; break;
+            case TransportationMethod.Tram: localizedString = tramString; break;
+            case TransportationMethod.Train: localizedString = trainString; break;
+            case TransportationMethod.Cart: localizedString = cartString; break;
+            case TransportationMethod.Ship: localizedString = shipString; break;
+            case TransportationMethod.Carriage: localizedString = carriageString; break;
+        }
+
+        return LocalizationManager.Instance.GetLocalizedString(localizedString);
+    }
+
     public void Init(string destination, IEnumerable<TransportationMethod> methods)
     {
-        string methodsString = methods.Select(method => method.ToString()).Aggregate((a, b) => $"{a}, {b}");
+        string methodsString = methods.Select(method => GetLocalizedStringForMethod(method)).Aggregate((a, b) => $"{a}, {b}");
         LocalizeStringEvent localizeStringEvent = title.GetComponent<LocalizeStringEvent>();
         localizeStringEvent.StringReference.Arguments = new object[] { destination, methodsString };
         localizeStringEvent.RefreshString();

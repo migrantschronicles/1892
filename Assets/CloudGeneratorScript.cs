4b93259a13a9b598a99662eaa2cd9788690995c0
KolibriAnimation <yadira.mrdesign@gmail.com>
2023-04-10 13:32:36 +0200

    Cloud Prefab
    Assets/Prefabs/EnvironmentAsssets/CloudGenerator

---
+++ Assets/CloudGeneratorScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGeneratorScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] clouds;
+
+    [SerializeField] 
+    float spawnInterval;
+
+    [SerializeField]
+    GameObject endPoint;
+
+    Vector3 startPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        Prewarm();
+        Invoke("AttemptSpawn", spawnInterval);
+
+    }
+
+    void SpawnCloud(Vector3 startPos)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, clouds.Length);
+        GameObject cloud = Instantiate(clouds[randomIndex]);
+
+        float startY = UnityEngine.Random.Range(startPos.y - 1f, startPos.y + 2.7f);
+        cloud.transform.position = new Vector3(startPos.x,startY,startPos.z);
+
+        float scale = UnityEngine.Random.Range(0.1f, 0.5f);
+        cloud.transform.localScale = new Vector2(scale, scale);
+
+
+        float speed = UnityEngine.Random.Range(0.15f, 0.6f);
+        cloud.GetComponent<CloudScript>().StartFloating(speed, endPoint.transform.position.x);
+    
+        
+    }
+
+    void AttemptSpawn()
+    {
+        SpawnCloud(startPos);
+
+        Invoke("AttemptSpawn", spawnInterval);
+    }
+
+    void Prewarm()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            Vector3 spawnPos = startPos + Vector3.right * (i * 2);
+            SpawnCloud(spawnPos);
+
+        }         
+    }
+}

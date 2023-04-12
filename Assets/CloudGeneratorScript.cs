0334d44f503dd67991891ec86b9abf4f2f53bb9d
Yannik Hupfer <yannikhupfer@gmail.com>
2023-04-12 11:59:06 +0200

    Integrated CloudGenerator into LevelInstance

--- Assets/CloudGeneratorScript.cs
+++ Assets/CloudGeneratorScript.cs
@@ -13,6 +13,9 @@ public class CloudGeneratorScript : MonoBehaviour
     [SerializeField]
     GameObject endPoint;
 
+    [SerializeField]
+    private int orderInLayer = 50;
+
     Vector3 startPos;
 
 
@@ -28,13 +31,22 @@ public class CloudGeneratorScript : MonoBehaviour
     void SpawnCloud(Vector3 startPos)
     {
         int randomIndex = UnityEngine.Random.Range(0, clouds.Length);
-        GameObject cloud = Instantiate(clouds[randomIndex]);
+        GameObject cloud = Instantiate(clouds[randomIndex], transform);
+        SpriteRenderer[] renderers = cloud.GetComponentsInChildren<SpriteRenderer>();
+        foreach(SpriteRenderer renderer in renderers)
+        {
+            renderer.sortingOrder = orderInLayer;
+        }
 
         float startY = UnityEngine.Random.Range(startPos.y - 1f, startPos.y + 2.7f);
         cloud.transform.position = new Vector3(startPos.x,startY,startPos.z);
 
         float scale = UnityEngine.Random.Range(0.1f, 0.5f);
-        cloud.transform.localScale = new Vector2(scale, scale);
+        Vector3 newScale = new Vector3(scale, scale);
+        newScale.x *= 1.0f / transform.localScale.x;
+        newScale.y *= 1.0f / transform.localScale.y;
+        newScale.z *= 1.0f / transform.localScale.z;
+        cloud.transform.localScale = newScale;
 
 
         float speed = UnityEngine.Random.Range(0.15f, 0.6f);

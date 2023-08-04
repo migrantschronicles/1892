075499602f7ed3aef4b0d5c1ca3e3634afcb6b6f
Loai Qubti <loai17@meet.mit.edu>
2023-08-04 18:24:54 +0200

    Main Menu, Ship zoom

---
+++ Assets/TeachingModePopup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeachingModePopup : MonoBehaviour
+{
+    public GameObject popupContainer;
+    public UnityEngine.UI.Button teachingModeButton;
+    public UnityEngine.UI.Button closeDialogueButton;
+    public UnityEngine.UI.Text text;
+    public Color color;
+
+    public delegate void OnClickDelegate();
+    public event OnClickDelegate onClick;
+
+    private void Start()
+    {
+        teachingModeButton.onClick.AddListener(ShowPopup);
+        closeDialogueButton.onClick.AddListener(HidePopup);
+    }
+
+    public void ShowPopup()
+    {
+        text.color = new Color(255, 255, 255);
+        popupContainer.SetActive(true);
+    }
+
+    public void HidePopup()
+    {
+        text.color = color;
+        popupContainer.SetActive(false);
+    }
+
+}
